faa90f93991300a497fdd8e96fc7a7f648b6c5d4
Andrea Bentley <andrea@bentleytech.com>
2020-04-27 22:52:48 -0400

    Added GitHub API call

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const api = require("./utils/api.js");
 // const util = require("util");
 
 // const writeFileAsync = util.promisify(fs.writeFile);
@@ -14,56 +15,56 @@ const questions = [
     type: "input",
     name: "repoName",
     message: "What is your repository name?"
-  },
-  {
-    type: "input",
-    name: "projTitle",
-    message: "What is your project title?"
-  },
-  {
-    type: "input",
-    name: "projDesc",
-    message: "What is your project description?"
-  },
-  {
-    type: "input",
-    name: "projUser",
-    message: "What type of person is your audience? (i.e. developer, accountant, teacher)"
-  },
-  {
-    type: "input",
-    name: "projNeed",
-    message: "What is your audience's need?"
-  },
-  {
-    type: "input",
-    name: "projResult",
-    message: "What is the target outcome for your audience?"
-  },
-  {
-    type: "input",
-    name: "projInstall",
-    message: "What are your project's installation instructions?"
-  },
-  {
-    type: "input",
-    name: "projLicense",
-    message: "What type of license does your project have?"
-  },
-  {
-    type: "input",
-    name: "projContribs",
-    message: "Enter the GitHub usernames of all contributors, separated by commas:"
-  },
-  {
-    type: "input",
-    name: "projTests",
-    message: "Project tests?"
-  },
-  {
-    type: "input",
-    name: "projQs",
-    message: "Project questions?"
+  // },
+  // {
+  //   type: "input",
+  //   name: "projTitle",
+  //   message: "What is your project title?"
+  // },
+  // {
+  //   type: "input",
+  //   name: "projDesc",
+  //   message: "What is your project description?"
+  // },
+  // {
+  //   type: "input",
+  //   name: "projUser",
+  //   message: "What type of person is your audience? (i.e. developer, accountant, teacher)"
+  // },
+  // {
+  //   type: "input",
+  //   name: "projNeed",
+  //   message: "What is your audience's need?"
+  // },
+  // {
+  //   type: "input",
+  //   name: "projResult",
+  //   message: "What is the target outcome for your audience?"
+  // },
+  // {
+  //   type: "input",
+  //   name: "projInstall",
+  //   message: "What are your project's installation instructions?"
+  // },
+  // {
+  //   type: "input",
+  //   name: "projLicense",
+  //   message: "What type of license does your project have?"
+  // },
+  // {
+  //   type: "input",
+  //   name: "projContribs",
+  //   message: "Enter the GitHub usernames of all contributors, separated by commas:"
+  // },
+  // {
+  //   type: "input",
+  //   name: "projTests",
+  //   message: "Project tests?"
+  // },
+  // {
+  //   type: "input",
+  //   name: "projQs",
+  //   message: "Project questions?"
   }
 ];
 
@@ -86,14 +87,23 @@ async function init() {
   console.log("\n Welcome to the README Generator \n");
   try {
     const answers = await promptUser();
-    const readme = `README-${answers.repoName}.md`;
-    let overwriteOkay = true;
 
-    if (fs.existsSync(readme)) {
-      overwriteOkay = verifyOverwrite(readme);
-    };
+    try {
+      const userInfo = await api.getUser(answers.userName);
+      console.log("userInfo: " + userInfo);
 
-    if (overwriteOkay) {
+      const readme = `README-${answers.repoName}.md`;
+      let overwriteOkay = true;
+
+      if (fs.existsSync(readme)) {
+        overwriteOkay = verifyOverwrite(readme);
+      };
+
+      if (overwriteOkay) {
+      }
+    }
+    catch(error) {
+      console.log(error);
     }
   } 
   catch(err) {
--- Develop/utils/api.js
+++ Develop/utils/api.js
@@ -1,6 +1,17 @@
+const axios = require("axios").default;
+
 const api = {
   getUser(username) {
-
+    axios.get("https://api.github.com/users/" + username)
+    .then(function (response) {
+      // handle success
+      console.log("Response: " + response.data);
+      return response.data;
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
   }
 };
 
